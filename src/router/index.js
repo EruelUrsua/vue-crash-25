4d893f7472cb79f50bb51c08c9949db8e0287fb5
EruelUrsua <eruelursua@gmail.com>
2025-04-29 20:05:57 +0800

    added new router

--- src/router/index.js
+++ src/router/index.js
@@ -1,2 +1,22 @@
-import { createRouter, CreateWebHistory } from 'vue-router';
+import { createRouter, createWebHistory} from 'vue-router';
 
+import HomeView from '@/views/HomeView.vue';
+import JobsView from '@/views/JobsView.vue';
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: HomeView,
+        },
+        {
+            path: '/jobs',
+            name: 'jobs',
+            component: JobsView,
+        },
+    ],
+});
+
+export default router;
